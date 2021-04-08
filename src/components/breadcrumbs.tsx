b19000261ae34a44d48f54422b801362c25411b6
ryohidaka <git@ryohidaka.jp>
2021-04-08 22:50:17 +0900

    update: パンくずリストを別コンポーネントに分離

---
+++ src/components/breadcrumbs.tsx
@@ -0,0 +1,32 @@
+import * as React from "react"
+import { Breadcrumbs, Link } from "@material-ui/core"
+
+type Props = {
+  crumbs: Crumb[]
+}
+
+type Crumb = {
+  pathname: string
+  crumbLabel: string
+}
+
+const Breadcrumb: React.FC<Props> = ({ crumbs }: Props) => {
+  return (
+    <Breadcrumbs aria-label="breadcrumb">
+      {crumbs.map((crumb: Crumb) => {
+        return (
+          <Link
+            color="inherit"
+            key={crumb.pathname}
+            href={crumb.pathname}
+            itemProp="url"
+          >
+            {crumb.crumbLabel}
+          </Link>
+        )
+      })}
+    </Breadcrumbs>
+  )
+}
+
+export default Breadcrumb
