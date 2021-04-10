f8d6e1bba65201ce82ece01b6c1bb625e61f356f
ryohidaka <git@ryohidaka.jp>
2021-04-10 19:44:00 +0900

    update: セクションタイトルのスタイルを修正

--- src/components/section-title.tsx
+++ src/components/section-title.tsx
@@ -1,12 +1,33 @@
 import React from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import { Typography } from "@material-ui/core"
 
 type Props = {
   title: string
   id?: string
 }
 
+const useStyles = makeStyles({
+  root: {
+    margin: "8rem auto",
+    textTransform: "uppercase",
+  },
+})
+
 const SectionTitle: React.FC<Props> = ({ title, id }: Props) => {
-  return <h1 id={id || ""}>{title}</h1>
+  const classes = useStyles()
+
+  return (
+    <Typography
+      variant="h2"
+      component="h2"
+      id={id || ""}
+      align="center"
+      className={classes.root}
+    >
+      {title}
+    </Typography>
+  )
 }
 
 export default SectionTitle
